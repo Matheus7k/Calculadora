9c0298698c4e910dc0314ba2c8e4e813ea29ac89
Matheus7k <06matheus.oliveira@gmail.com>
2023-03-03 15:52:20 -0300

    Adicionado menu para a calculadora

--- calculadora/Program.cs
+++ calculadora/Program.cs
@@ -1,21 +1,58 @@
-﻿internal class Program
+﻿double num1, num2;
+int opcao = 0;
+
+void Menu(double num1, double num2)
 {
-    private static void Main(string[] args)
+    Console.Clear();
+    Console.WriteLine("1 - Somar");
+    Console.WriteLine("2 - Subtrair");
+    Console.WriteLine("3 - Multiplicar");
+    Console.WriteLine("4 - Dividir");
+    Console.WriteLine("5 - Sair");
+    Console.Write("Escolha uma opçao: ");
+    opcao = int.Parse(Console.ReadLine());
+
+    switch (opcao)
     {
-        float n1, n2;
-
-        Console.WriteLine("Informe o primeiro numero: ");
-        n1 = float.Parse(Console.ReadLine());
-        Console.WriteLine("Informe o segundo numero: ");
-        n2 = float.Parse(Console.ReadLine());
-
-        Console.WriteLine($"A soma dos numeros é: {n1 + n2}");
-        Console.WriteLine($"A subtração dos numeros é: {n1 - n2}");
-        Console.WriteLine($"A multiplicação dos numeros é: {n1 * n2}");
-
-        if (n2 == 0)
-        {
-            Console.WriteLine("Impossivel dividir por zero!");
-        }else Console.WriteLine($"A divisão dos numeros é: {n1/n2}");
+        case 1:
+            Console.WriteLine($"A soma dos numeros é: {num1 + num2}");
+            Console.ReadLine();
+            break;
+        case 2:
+            Console.WriteLine($"A subtração dos numeros é: {num1 - num2}");
+            Console.ReadLine();
+            break;
+        case 3:
+            Console.WriteLine($"A multiplicação dos numeros é: {num1 * num2}");
+            Console.ReadLine();
+            break;
+        case 4:
+            if(num2 == 0)
+            {
+                Console.WriteLine("Impossivel dividir por zero!");
+                Console.ReadLine();
+                break;
+            }
+            Console.WriteLine($"A divisão dos numeros é: {num1 / num2}");
+            Console.ReadLine();
+            break;
+        case 5:
+            Console.WriteLine($"Até mais!");
+            Console.ReadLine();
+            break;
+        default:
+            Console.WriteLine("Opção inválida!");
+            Console.ReadLine();
+            break;
     }
+}
+
+Console.Write("Informe o primeiro numero: ");
+num1 = double.Parse(Console.ReadLine());
+Console.Write("Informe o segundo numero: ");
+num2 = double.Parse(Console.ReadLine());
+
+while(opcao != 5)
+{
+    Menu(num1, num2);
 }
