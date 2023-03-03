310c00a0c784bb1ead253975c0a4275be21951b1
Matheus7k <06matheus.oliveira@gmail.com>
2023-03-03 15:09:00 -0300

    Add project files.

---
+++ calculadora/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        float n1, n2;
+
+        Console.WriteLine("Informe o primeiro numero: ");
+        n1 = float.Parse(Console.ReadLine());
+        Console.WriteLine("Informe o segundo numero: ");
+        n2 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine($"A soma dos numeros é: {n1 + n2}");
+        Console.WriteLine($"A subtração dos numeros é: {n1 - n2}");
+        Console.WriteLine($"A multiplicação dos numeros é: {n1 * n2}");
+
+        if (n2 == 0)
+        {
+            Console.WriteLine("Impossivel dividir por zero!");
+        }else Console.WriteLine($"A divisão dos numeros é: {n1/n2}");
+    }
+}
