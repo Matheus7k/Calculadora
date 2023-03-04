afa90169872c6e83b9bd88256cb9b8b7ba47c0f0
Matheus7k <06matheus.oliveira@gmail.com>
2023-03-04 18:46:10 -0300

    Adicionado funções de soma. subtração, multiplicação e divisão

--- calculadora/Program.cs
+++ calculadora/Program.cs
@@ -1,7 +1,7 @@
 ﻿double num1, num2;
 int opcao = 0;
 
-void Menu(double num1, double num2)
+int Menu()
 {
     Console.Clear();
     Console.WriteLine("1 - Somar");
@@ -10,30 +10,60 @@ void Menu(double num1, double num2)
     Console.WriteLine("4 - Dividir");
     Console.WriteLine("5 - Sair");
     Console.Write("Escolha uma opçao: ");
-    opcao = int.Parse(Console.ReadLine());
+    return int.Parse(Console.ReadLine());
+}
+
+double Soma()
+{
+    return num1 + num2;
+}
+
+double Subtracao()
+{
+    return num1 - num2;
+}
+
+double Multiplicacao()
+{
+    return num1 * num2;
+}
+
+double Divisao()
+{
+    return num1 / num2;
+}
+
+Console.Write("Informe o primeiro numero: ");
+num1 = double.Parse(Console.ReadLine());
+Console.Write("Informe o segundo numero: ");
+num2 = double.Parse(Console.ReadLine());
+
+while(opcao != 5)
+{
+    opcao = Menu();
 
     switch (opcao)
     {
         case 1:
-            Console.WriteLine($"A soma dos numeros é: {num1 + num2}");
+            Console.WriteLine($"A soma dos numeros é: {Soma()}");
             Console.ReadLine();
             break;
         case 2:
-            Console.WriteLine($"A subtração dos numeros é: {num1 - num2}");
+            Console.WriteLine($"A subtração dos numeros é: {Subtracao()}");
             Console.ReadLine();
             break;
         case 3:
-            Console.WriteLine($"A multiplicação dos numeros é: {num1 * num2}");
+            Console.WriteLine($"A multiplicação dos numeros é: {Multiplicacao()}");
             Console.ReadLine();
             break;
         case 4:
-            if(num2 == 0)
+            if (num2 == 0)
             {
                 Console.WriteLine("Impossivel dividir por zero!");
                 Console.ReadLine();
                 break;
             }
-            Console.WriteLine($"A divisão dos numeros é: {num1 / num2}");
+            Console.WriteLine($"A divisão dos numeros é: {Divisao()}");
             Console.ReadLine();
             break;
         case 5:
@@ -45,14 +75,4 @@ void Menu(double num1, double num2)
             Console.ReadLine();
             break;
     }
-}
-
-Console.Write("Informe o primeiro numero: ");
-num1 = double.Parse(Console.ReadLine());
-Console.Write("Informe o segundo numero: ");
-num2 = double.Parse(Console.ReadLine());
-
-while(opcao != 5)
-{
-    Menu(num1, num2);
 }
