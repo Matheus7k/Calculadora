c765c57a944a5a18ba43e631789f5be465f169ac
Matheus7k <06matheus.oliveira@gmail.com>
2023-03-06 10:26:00 -0300

    Melhorado estrutura do while e switch

--- calculadora/Program.cs
+++ calculadora/Program.cs
@@ -1,5 +1,5 @@
 ﻿double num1, num2;
-int opcao = 0;
+int continua = 1;
 
 int Menu()
 {
@@ -38,41 +38,35 @@ num1 = double.Parse(Console.ReadLine());
 Console.Write("Informe o segundo numero: ");
 num2 = double.Parse(Console.ReadLine());
 
-while(opcao != 5)
+while(continua == 1)
 {
-    opcao = Menu();
-
-    switch (opcao)
+    switch (Menu())
     {
         case 1:
-            Console.WriteLine($"A soma dos numeros é: {Soma()}");
-            Console.ReadLine();
+            Console.WriteLine($"A soma dos numeros é: {Soma()}");           
             break;
         case 2:
-            Console.WriteLine($"A subtração dos numeros é: {Subtracao()}");
-            Console.ReadLine();
+            Console.WriteLine($"A subtração dos numeros é: {Subtracao()}");           
             break;
         case 3:
-            Console.WriteLine($"A multiplicação dos numeros é: {Multiplicacao()}");
-            Console.ReadLine();
+            Console.WriteLine($"A multiplicação dos numeros é: {Multiplicacao()}");           
             break;
         case 4:
             if (num2 == 0)
             {
-                Console.WriteLine("Impossivel dividir por zero!");
-                Console.ReadLine();
+                Console.WriteLine("Impossivel dividir por zero!");               
                 break;
             }
-            Console.WriteLine($"A divisão dos numeros é: {Divisao()}");
-            Console.ReadLine();
+            Console.WriteLine($"A divisão dos numeros é: {Divisao()}");          
             break;
         case 5:
-            Console.WriteLine($"Até mais!");
-            Console.ReadLine();
+            Console.WriteLine($"Até mais!");          
+            continua = 0;
             break;
         default:
-            Console.WriteLine("Opção inválida!");
-            Console.ReadLine();
+            Console.WriteLine("Opção inválida!");           
             break;
     }
+    Console.WriteLine("Pressione a tecla ENTER para continuar!");
+    Console.ReadLine();
 }
